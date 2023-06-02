e06f91e035aecc80169545bb6c17864372d721d3
PabloCastro <pablocastrod7@gmail.com>
2023-06-02 00:52:07 -0300

    Agregando mas validaciones

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
-const API_KEY = '1f7643e64c74201ca143813e036a9d9b020c50f9f23306dde32a7652e5ce6b752927d83c8';
-const API_URL = 'https://gateway.marvel.com:443/v1/public/series?ts=1&apikey=23306dde32a7652e5ce6b752927d83c8&hash=0cb3651037b4d8f9cd7fe8e5f1de8d51'
-
+const API_KEY =
+   "1f7643e64c74201ca143813e036a9d9b020c50f9f23306dde32a7652e5ce6b752927d83c8";
+const API_URL =
+   "https://gateway.marvel.com:443/v1/public/series?ts=1&apikey=23306dde32a7652e5ce6b752927d83c8&hash=0cb3651037b4d8f9cd7fe8e5f1de8d51";
 
 fetch(API_URL)
-        .then(res=>res.json())
-        .then(json=>{
-                const series = json.data.results;
-                        const allSeries = document.getElementById('series');
-                        series.forEach((serie) => {
-                                const serieElement = document.createElement('div');
-                                
-                                serieElement.classList.add('one_serie');
-                                serieElement.innerHTML = `
+   .then((res) => res.json())
+   .then((json) => {
+      const series = json.data.results;
+      const allSeries = document.getElementById("series");
+      series.forEach((serie) => {
+         const serieElement = document.createElement("div");
+
+         serieElement.classList.add("one_serie");
+         serieElement.innerHTML = `
                                         <div class="serie_img">
                                                 <img src="${serie.thumbnail.path}.${serie.thumbnail.extension}" alt="${serie.title}">
                                         </div>
@@ -21,6 +22,6 @@ fetch(API_URL)
                                                 <button><a href='#'>Mas información</a></button>
                                         </div>
                                 `;
-                                allSeries.appendChild(serieElement);
-                        });
-        });
+         allSeries.appendChild(serieElement);
+      });
+   });
--- js/validate.js
+++ js/validate.js
@@ -1,29 +1,85 @@
-const form = document.getElementById('form')
+const form = document.getElementById("form");
 
+form.addEventListener("submit", (e) => {
+   e.preventDefault();
 
+   const nombre = document.getElementById("nombre").value;
+   const apellido = document.getElementById("apellido").value;
+   const direccion = document.getElementById("direccion").value;
+   const telefono = document.getElementById("telefono").value;
+   const email = document.getElementById("email").value;
 
-form.addEventListener('submit', e => {
-    e.preventDefault()
-    
-    const nombre = document.getElementById('nombre').value
-    const apellido = document.getElementById('apellido').value
-    const direccion = document.getElementById('direccion').value
-    const telefono = document.getElementById('telefono').value
-    const email = document.getElementById('email').value 
+   // Regexp
+   var regName = /^[a-zA-Z ]+$/;
+   var regDireccion = /^[a-zA-Z0-9\s,'-]*$/;
+   var regTelefono = /^[0-9]+$/;
+   var regEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.([a-zA-Z]{2,4})+$/;
 
-    if(nombre === '' && apellido === '' && direccion === '' && telefono === ''){
-        Swal.fire({
-            title: 'Cuidado !',
-            text: 'Tiene que completar todos los campos',
-            icon: 'warning',
-            confirmButtonText: 'Reintentar'
-            })
-    }else{
-        Swal.fire({
-            title: 'Datos enviados !',
-            icon: 'succes',
-            confirmButtonText: 'Okay'
-        })
-    }
-    
-})
+   if (
+      nombre === "" ||
+      apellido === "" ||
+      direccion === "" ||
+      telefono === "" ||
+      email === ""
+   ) {
+      Swal.fire({
+         title: "Cuidado !",
+         text: "Tenes que completar todos los campos",
+         icon: "warning",
+         confirmButtonText: "Reintentar",
+      });
+   } else if (nombre.length > 10) {
+      Swal.fire({
+         title: "Cuidado !",
+         text: "El nombre no puede tener mas de 10 caracteres",
+         icon: "warning",
+         confirmButtonText: "Reintentar",
+      });
+   } else if (apellido.length > 10) {
+      Swal.fire({
+         title: "Cuidado !",
+         text: "El apellido no puede tener mas de 10 caracteres",
+         icon: "warning",
+         confirmButtonText: "Reintentar",
+      });
+   } else if (!regName.test(nombre) || !regName.test(apellido)) {
+      Swal.fire({
+         title: "Cuidado !",
+         text: "El nombre y el apellido tienen que ser alfabeticos",
+         icon: "warning",
+         confirmButtonText: "Reintentar",
+      });
+   } else if (!regTelefono.test(telefono)) {
+      Swal.fire({
+         title: "Cuidado !",
+         text: "El telefono tiene que ser numerico",
+         icon: "warning",
+         confirmButtonText: "Reintentar",
+      });
+   } else if (!regDireccion.test(direccion)) {
+      Swal.fire({
+         title: "Cuidado !",
+         text: "La direccion tiene que ser alfanumerica",
+         icon: "warning",
+         confirmButtonText: "Reintentar",
+      });
+   } else if (!regEmail.test(email)) {
+      Swal.fire({
+         title: "Cuidado !",
+         text: "El email tiene que ser valido",
+         icon: "warning",
+         confirmButtonText: "Reintentar",
+      });
+   } else {
+      Swal.fire({
+         title: "Datos enviados !",
+         icon: "succes",
+         confirmButtonText: "Okay",
+      });
+   }
+   nombre = "";
+   apellido = "";
+   direccion = "";
+   telefono = "";
+   email = "";
+});
